2fb89ec28a45f6cb259d33556095bbdcf9bbbb2b
Yash Srivastava <lightsaber2005genius@gmail.com>
2025-12-23 02:35:17 +0530

    added report_generators package with PDF generation capability

---
+++ report_generators/__init__.py
@@ -0,0 +1,9 @@
+"""
+report_generators package
+
+Report generation utilities (PDF, CSV, etc.)
+"""
+
+from report_generators.pdf_generator import PDFReportGenerator
+
+__all__ = ["PDFReportGenerator"]
